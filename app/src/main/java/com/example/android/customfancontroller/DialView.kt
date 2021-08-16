28181769c5e2292212a1ebce9054b82d21c9e5a3
cstimler <charles.ces@gmail.com>
2021-08-16 00:48:07 -0700

    Custom fan controller complete with actions such as changing fan speeds on click and changing color of button.

--- app/src/main/java/com/example/android/customfancontroller/DialView.kt
+++ app/src/main/java/com/example/android/customfancontroller/DialView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.withStyledAttributes
 import kotlin.math.cos
 import kotlin.math.min
 import kotlin.math.sin
@@ -13,6 +14,13 @@ private enum class FanSpeed(val label: Int) {
     LOW(R.string.fan_low),
     MEDIUM(R.string.fan_medium),
     HIGH(R.string.fan_high);
+
+    fun next() = when (this) {
+        OFF -> LOW
+        LOW -> MEDIUM
+        MEDIUM -> HIGH
+        HIGH -> OFF
+    }
 }
 
 private const val RADIUS_OFFSET_LABEL = 30
@@ -36,6 +44,34 @@ private const val RADIUS_OFFSET_INDICATOR = -35
             typeface = Typeface.create("", Typeface.BOLD)
         }
 
+        private var fanSpeedLowColor = 0
+        private var fanSpeedMediumColor = 0
+        private var fanSpeedMaxColor = 0
+
+
+
+        init {
+            isClickable = true
+
+            context.withStyledAttributes(attrs, R.styleable.DialView) {
+                fanSpeedLowColor = getColor(R.styleable.DialView_fanColor1, 0)
+                fanSpeedMediumColor = getColor(R.styleable.DialView_fanColor2, 0)
+                fanSpeedMaxColor = getColor(R.styleable.DialView_fanColor3, 0)
+            }
+
+                }
+
+
+
+        override fun performClick(): Boolean {
+            if (super.performClick()) return true
+
+            fanSpeed = fanSpeed.next()
+            contentDescription = resources.getString(fanSpeed.label)
+            invalidate()
+            return true
+        }
+
         override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
             radius = (min(width, height)/2.0 * 0.8).toFloat()
     }
@@ -50,8 +86,12 @@ private const val RADIUS_OFFSET_INDICATOR = -35
         override fun onDraw(canvas: Canvas) {
             super.onDraw(canvas)
             // Set dial background color to green if selection not off.
-            paint.color = if (fanSpeed == FanSpeed.OFF) Color.GRAY else Color.GREEN
-            // Draw the dial.
+            paint.color = when (fanSpeed) {
+                FanSpeed.OFF -> Color.GRAY
+                FanSpeed.LOW -> fanSpeedLowColor
+                FanSpeed.MEDIUM -> fanSpeedMediumColor
+                FanSpeed.HIGH  -> fanSpeedMaxColor
+            } as Int
             canvas.drawCircle((width / 2).toFloat(), (height / 2).toFloat(), radius, paint)
             // Draw the indicator circle.
             val markerRadius = radius + RADIUS_OFFSET_INDICATOR
